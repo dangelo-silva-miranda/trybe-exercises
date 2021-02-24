1ef2b2ff8b982e7e971ef2a8250db131a1d6263e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-02-24 18:27:19 -0300

    Commit inicial e exercicio da parte II sobre let e const

---
+++ exercises/bloco_4/dia_1/conteudo.js
@@ -0,0 +1,14 @@
+const name = "Dângelo";
+const birthCity = "Ipatinga-MG";
+let birthYear = 1984;
+
+console.log(`Name: ${name}`);
+console.log("birthCity: "+birthCity);
+console.log("birthYear: "+birthYear);
+
+birthYear = 2020;
+console.log("birthYear: "+birthYear);
+
+birthCity = "Belo Horizonte-MG";
+console.log("birthCity: "+birthCity);
+/* Como birthCity é uma variável do tipo constante não pode ser redefinida. */
