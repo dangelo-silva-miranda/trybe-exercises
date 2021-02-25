4c7fea564f014dded2177bf4585b65b3c0e6a2c1
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-02-25 15:56:03 -0300

    Exercício da parte III sobre os tipos de variáveis e operadores aritméticos

--- exercises/bloco_4/dia_1/conteudo.js
+++ exercises/bloco_4/dia_1/conteudo.js
@@ -9,6 +9,40 @@ console.log("birthYear: "+birthYear);
 birthYear = 2020;
 console.log("birthYear: "+birthYear);
 
+/*
 birthCity = "Belo Horizonte-MG";
 console.log("birthCity: "+birthCity);
-/* Como birthCity é uma variável do tipo constante não pode ser redefinida. */
+ Como birthCity é uma variável do tipo constante não pode ser redefinida. */
+
+ let patientId = 50;
+ let isEnrolled = true;
+ const patientInfo = {
+   firstName: 'Ana',
+   lastName: 'Santos',
+ };
+ const patientEmail = 'ana@email.com';
+
+ console.log("patientId: "+typeof patientId);
+
+ console.log("isEnrolled: "+typeof isEnrolled);
+
+ console.log("patientInfo: "+typeof patientInfo);
+
+console.log("patientEmail: "+typeof patientEmail);
+
+ console.log("patientAge: "+typeof patientAge);
+
+ patientId = '50';
+
+ console.log("patientId: "+typeof patientId);
+
+ let base = 5;
+ let altura = 8;
+ console.log('base: ' + base);
+ console.log('altura: ' + altura);
+
+ let area =  base * altura;
+ console.log('area: ' + area);
+
+ let perimetro = 2 * (base + altura);
+ console.log('perimetro: ' + perimetro);
