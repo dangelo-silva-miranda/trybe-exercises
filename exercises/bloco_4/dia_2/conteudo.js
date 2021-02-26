48daad7c2762b6253c5a87dd844fd326a3fdfc00
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-02-26 15:02:17 -0300

    Estudo do conteúdo sobre arrays (listas) até push

---
+++ exercises/bloco_4/dia_2/conteudo.js
@@ -0,0 +1,17 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
